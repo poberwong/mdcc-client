bedbb2a6a9a81c9ae2c32ff1e9b2e0625a636979
bob <bob1993.dev@gmail.com>
2016-09-20 00:49:01 +0800

    enhance AboutPage

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {PropTypes, Component} from 'react'
 import {
   View,
   Text,
@@ -9,37 +9,48 @@ import {
   Image
 } from 'react-native'
 
-export default (props) => {
-  return (
-    <View style={{flex: 1, backgroundColor: 'white'}}>
-      <StatusBar barStyle='default'/>
-      <Image source={require('../assets/location.jpg')}
-        style={{width: null, height: 264}}/>
-      <View style={{padding: 15, flex: 1, alignItems: 'center'}}>
-        <Text style={{fontSize: 23, fontWeight: 'bold', lineHeight: 25, color: '#555', textAlign: 'center'}}>Thanks for contribution from
-          liaohuqiu and poberwong</Text>
-        <Text style={{fontSize: 13, lineHeight: 18, color: '#888', textAlign: 'center', marginTop: 25}}>
-          This application based on react-native-cx is {'\n'}  made for mdcc conference.
-          You can find {'\n'} it on github from bottom address.
-        </Text>
-        <View style={{flexDirection: 'row', flex: 1, alignItems: 'center'}}>
-          <Image source={require('../assets/avatar.png')} style={[styles.avatar, {backgroundColor: 'white'}]}/>
-          <Image source={require('../assets/liaohuqiu.jpeg')} style={[styles.avatar, {marginLeft: -15, zIndex: -1}]}/>
+export default class extends Component {
+  static propTypes = {
+    updateTime: PropTypes.string,
+    navigator: PropTypes.object
+  };
+
+  render () {
+    const {navigator, updateTime} = this.props
+    return (
+      <View style={{flex: 1, backgroundColor: 'white'}}>
+        <StatusBar barStyle='default'/>
+        <Image source={require('../assets/location.jpg')}
+          style={{width: null, height: 264}}/>
+        <View style={{padding: 15, flex: 1, alignItems: 'center'}}>
+          <Text style={{fontSize: 23, fontWeight: 'bold', lineHeight: 25, color: '#555', textAlign: 'center'}}>Thanks for contribution from
+            liaohuqiu and poberwong</Text>
+          <Text style={{fontSize: 13, lineHeight: 18, color: '#888', textAlign: 'center', marginTop: 25}}>
+            This application based on react-native-cx is {'\n'}  made for mdcc conference.
+            You can find {'\n'} it on github from bottom address.
+          </Text>
+          <View style={{flexDirection: 'row', flex: 1, alignItems: 'center'}}>
+            <Image source={require('../assets/avatar.png')} style={[styles.avatar, {backgroundColor: 'white'}]}/>
+            <Image source={require('../assets/liaohuqiu.jpeg')} style={[styles.avatar, {marginLeft: -15, zIndex: -1}]}/>
+          </View>
+          <View style={styles.bottom}>
+            <Text style={[styles.bottomText, {marginBottom: 5}]}>latest_update: {updateTime}</Text>
+            <TouchableOpacity
+              style={{flexDirection: 'row'}}
+              onPress={() => Linking.openURL('https://github.com/Bob1993/mdcc-client')}>
+                <Image
+                  style={styles.github}
+                  source={{uri: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Octicons-mark-github.svg/1200px-Octicons-mark-github.svg.png'}} />
+                <Text style={styles.bottomText}>https://github.com/Bob1993/mdcc-client</Text>
+            </TouchableOpacity>
+          </View>
         </View>
-        <View style={styles.bottom}>
-          <Text style={[styles.bottomText, {marginBottom: 5}]}>latest_update: {props.updateTime}</Text>
-          <TouchableOpacity
-            style={{flexDirection: 'row'}}
-            onPress={() => Linking.openURL('https://github.com/Bob1993/mdcc-client')}>
-              <Image
-                style={styles.github}
-                source={{uri: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Octicons-mark-github.svg/1200px-Octicons-mark-github.svg.png'}} />
-              <Text style={styles.bottomText}>https://github.com/Bob1993/mdcc-client</Text>
-          </TouchableOpacity>
-        </View>
+        <TouchableOpacity
+          hitSlop={{top: 15, left: 15, bottom: 15, right: 15}}
+          style={styles.backArrow} onPress={() => navigator.pop()}/>
       </View>
-    </View>
-  )
+    )
+  }
 }
 
 const styles = StyleSheet.create({
@@ -52,6 +63,17 @@ const styles = StyleSheet.create({
     width: 15,
     height: 15,
     marginRight: 8
+  },
+  backArrow: {
+    width: 13,
+    height: 13,
+    borderColor: 'white',
+    borderLeftWidth: 2,
+    borderBottomWidth: 2,
+    transform: [{rotate: '45deg'}],
+    position: 'absolute',
+    left: 15,
+    top: 34
   },
   bottom: {
     position: 'absolute',
